9ed7ad02229ae073e63132237b5933f3f34ac90b
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-30 21:20:50 -0700

    implements solution for the activity

--- src/index.js
+++ src/index.js
@@ -10,21 +10,30 @@ const AsyncCalculator = require('./AsyncCalculator.js');
 
 // We can pass a delay in ms to the constructor to simulate a timeout
 
-const acSlow = new AsyncCalculator(1000);
+const ac = new AsyncCalculator(1000);
 
 //
 // 1. No output, and we don't know when this finishes. The add method
 //    returns a Promise for the sum, not the sum itself
 ////////////////////////////////////////////////////////////////////////
 
-console.log(acSlow.add(1, 2));  // just prints Promise { ... }, not the result
+console.log(ac.add(1, 2));  // just prints Promise { ... }, not the result
 
 //
 // 2. register a callback that runs when the add Promise completes
 //////////////////////////////////////////////////////////////////////
+const axios = require('axios');
+axios
+  ac.add(1, 3)
+  .then((response)=>{
+    console.log('It passed!');
+  })
+  .catch((error)=>{
+    console.log('It failed!');
+  });
 
 // to get the result, we must chain a then callback
-acSlow.add(1, 2).then(sum => {
+ac.add(1, 2).then(sum => {
   console.log(sum);  // prints 3, but not until after the delay
 });
 
@@ -34,17 +43,17 @@ console.log('faster than sum.');  // this will print before the prior result
 // 3. a more involved calculation: 1 + 2 + 3 + 4 + 5
 ///////////////////////////////////////////////////////////////////
 
-acSlow.add(1, 2).then(sum => {
-  return acSlow.add(sum, 3);  // note that we MUST return the value from
+ac.add(1, 2).then(sum => {
+  return ac.add(sum, 3);  // note that we MUST return the value from
   // our then callback that we want to get
   // passed to the next then callback.
 }).then(sum => {
-  return acSlow.add(sum, 4);  // if the value we return is a Promise, the
+  return ac.add(sum, 4);  // if the value we return is a Promise, the
   // next then will not be called until the
   // Promise completes, and the value passed
   // to the then callback with be the value
   // the Promise resolved to
-}).then(sum => acSlow.add(sum, 5))  // due to short syntax rules, this is
+}).then(sum => ac.add(sum, 5))  // due to short syntax rules, this is
 // the same as the two prior longer calls
   .then(sum => {
     console.log(sum);  // prints 15, but not until after 4 * delay
@@ -58,9 +67,9 @@ console.log('still faster than sum.');  // this will still print before the
 // 4. a calculation that results in an error (sqrt of a negative)
 ///////////////////////////////////////////////////////////////////
 
-acSlow.mult(2, 4).then(product => acSlow.mult(product, -1))  // multiply by -1 to turn
+ac.mult(2, 4).then(product => ac.mult(product, -1))  // multiply by -1 to turn
 // the result negative
-  .then(negative => acSlow.sqrt(negative))  // can we take the sqrt of a negative?
+  .then(negative => ac.sqrt(negative))  // can we take the sqrt of a negative?
   .then(maybeResult => {
     console.log({ maybeResult });  // if yes, we'll print here
   }).catch(err => {
@@ -81,11 +90,11 @@ acSlow.mult(2, 4).then(product => acSlow.mult(product, -1))  // multiply by -1 t
 // find (1 + 2) * (3 + 4)
 // we need the sum of 1 and 2, AND the sum of 3 and 4, before we can multiply
 
-Promise.all([acSlow.add(1, 2), acSlow.add(3, 4)])  // these can happen at the same time!
+Promise.all([ac.add(1, 2), ac.add(3, 4)])  // these can happen at the same time!
   .then(result => {
     const sum1 = result[0];  // this could be simplified with destructuring
     const sum2 = result[1];
-    return acSlow.mult(sum1, sum2);
+    return ac.mult(sum1, sum2);
   }).then(result => {
     console.log({ result });
   });
@@ -96,6 +105,7 @@ Promise.all([acSlow.add(1, 2), acSlow.add(3, 4)])  // these can happen at the sa
 
 // Find the longest side of a triangle with sides
 // of length 3 and 4
+//3*3 + 4*4 = c
 
 // Use the Pythagorean Theorem: a^2 + b^2 = c^2
 // so c = sqrt(a*a + b*b)
@@ -103,7 +113,18 @@ Promise.all([acSlow.add(1, 2), acSlow.add(3, 4)])  // these can happen at the sa
 const distanceAsync = (a, b) => {
   const ac = new AsyncCalculator();  // no delay, so as fast as possible,
   // but still async
+  Promise.all([ac.mult(a, a), ac.mult(b, b)])
+    .then(result =>{
+      const multA = result[0];
+      const multB = result[1];
+      return ac.add(multA, multB);
+    }).then(result => {
+      return ac.sqrt(result)
+    }).then(result => {
+      console.log({result});
+    });
 
   // TODO: find the distance
 };
+distanceAsync(4, 5);
 
